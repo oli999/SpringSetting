141d089e1bff97bb600545fca08d5100bd63c04b
kimgura <oli999@naver.com>
2023-01-06 17:45:11 +0900

    1. war 파일로 빌드해서 외부 tomcat 으로 실행할 준비함. 2. upload 폴더에 업로드된 파일을 git 에서 무시 하도록 .gitignore 파일 수정함

--- SpringBoot_MyBatis/src/main/java/com/acorn/basic/SpringBootMyBatisApplication.java
+++ SpringBoot_MyBatis/src/main/java/com/acorn/basic/SpringBootMyBatisApplication.java
@@ -2,12 +2,19 @@ package com.acorn.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootMyBatisApplication {
+public class SpringBootMyBatisApplication extends SpringBootServletInitializer{//war 파일 빌드를 위해
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMyBatisApplication.class, args);
 	}
-
+	//war 파일로 build 해서 외부 tomcat 으로 실행하려면 아래처럼 메소드를 오버라이드 해야한다. 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SpringBootMyBatisApplication.class);
+	}
 }
